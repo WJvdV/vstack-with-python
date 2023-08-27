6c8b9d7aaa7596922202edfe0ff9612131018a81
WJvdV <74607494+WJvdV@users.noreply.github.com>
2023-08-27 22:11:44 +0200

    Update vstack.py

--- vstack.py
+++ vstack.py
@@ -1,1 +1,32 @@
+'''
+Wim van der Veer, augustus 2023
+https://github.com/WJvdV
+-------------------------------
+openpyxl 3.1.0
+'''
 
+import openpyxl
+
+wb = openpyxl.load_workbook(filename="vstack_with_python.xlsx", read_only=False)
+
+sheets = wb.sheetnames
+rows = wb[sheets[0]].rows
+headers = [cell.value for cell in next(rows)]
+
+result = []
+i = 0
+
+for sheet in sheets:
+    maxCol = wb[ sheets[i] ].max_column
+    maxRow = wb[ sheets[i] ].max_row
+    for data in wb[sheets[i]].iter_rows(min_row=2, max_col=maxCol, max_row=maxRow, values_only=True):
+        result.append(data)
+    i = i + 1
+
+wb.create_sheet("combined", 0).append(headers)
+
+for row in result:
+    wb['combined'].append(row)
+
+wb.save("vstack_with_python_new.xlsx")
+wb.close()
